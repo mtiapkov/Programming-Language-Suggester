14d990dd838eaa847ab180a3d346b1db487e4c0d
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-06-05 14:19:17 -0700

    add working business logic to JS to show proper answers, added details to answer divs, linked outside websites to answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
 $(document).ready(function(){
   $("form#info").submit(function(event){
-    const beach = $("beachWalk").val();
-    const yes = $(".yes").val();
-    const no = $(".no").val();
-    const fires = $(".firesOut").val();
-    const puzzles = $(".puzzleSolve").val();
-    const confusion = $(".confusedState").val();
+    const beach = $("#beachWalk").val();
+    const fires = $("#firesOut").val();
+    const puzzles = $("#puzzleSolve").val();
+    const confusion = $("#confusedState").val();
     const age = parseInt($("input#age").val());
-    
-    if (beach  === "Yes"){
-      $(".answer1").show()
-      
+    if (!age){
+      alert("Please input your age in numbers");
+    }
+    else if(age > 120 || age < 0){
+      alert("The Programming Language Suggester 5000 only provides suggestions for those who are alive. Please enter a living age.")
+    }
+    if ((beach === "Yes") && (fires === "Yes") && (puzzles === "Yes") && (confusion === "Yes")){
+      $(".answer1").show();
+    } else if ((beach === "No") && (fires === "No") && (puzzles === "No") && (confusion === "No")){
+      $(".answer2").show();
+    } else {
+      $(".answer3").show();
+    }
     
     event.preventDefault()
-  }
+  
   });
 });
 
